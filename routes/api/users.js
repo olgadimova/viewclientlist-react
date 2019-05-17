425391652537186303a107fe9e34641c6b9777f4
olga <olgadimovaa@gmail.com>
2019-05-17 18:57:53 +0900

    new

--- routes/api/users.js
+++ routes/api/users.js
@@ -38,6 +38,44 @@ router.get("/", function(req,res) {
       });
     });
 
+    router.post("/manreguser", (req,res) => {
+      // Form validation
+    
+      const {errors, isValid} = validateRegisterInput(req.body);
+    
+      //CHeck validation 
+      if(!isValid) {
+          return res.status(400).json(errors);
+      }
+    
+      User.findOne({email: req.body.email}).then(user => {
+          if(user) {
+              return res.status(400).json({email: "Email already exists"})
+          } else {
+          
+          const newUser = new User({
+              name: req.body.name,
+              email: req.body.email,
+              password: req.body.password,
+              resetPasswordToken: "",
+              resetPasswordExpires: "",
+              customers: {}
+          });
+    
+          // Hash password before saving in database
+          bcrypt.genSalt(10, (err, salt) => {
+              bcrypt.hash(newUser.password, salt, (err,hash) => {
+                  if(err) throw err;
+                  newUser.password = hash;
+                  newUser
+                  .save()
+                  .then(user => res.json(user))
+                  .catch(err => console.log(err));
+              });
+          });
+      }
+      });
+    });
 
 // @route POST api/users/login
 // @desc Login user and return JWT token
