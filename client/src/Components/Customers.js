5cfee7dcc254b2d3fd1383ecb2e6cb2705481f2b
olga <olgadimovaa@gmail.com>
2019-05-17 19:03:21 +0900

    fix

--- client/src/Components/Customers.js
+++ client/src/Components/Customers.js
@@ -1,10 +1,6 @@
 import React,  { Component } from 'react';
 import { Link } from 'react-router-dom';
-import axios from'axios';
-
 import {CSVLink} from 'react-csv';
-import jsPDF from 'jspdf';
-import 'jspdf-autotable';
 
 
 
