2c696fb0517131fd9864601bd350c409cdc88b80
olga <olgadimovaa@gmail.com>
2019-05-23 10:49:01 +0900

    n

--- client/src/Components/Auth/Login.js
+++ client/src/Components/Auth/Login.js
@@ -11,6 +11,7 @@ class Login extends Component {
         this.state = {
             email: "",
             password: "",
+            errors: ""
         };
         this.onSumbit=this.onSubmit.bind(this);
         this.onChange=this.onChange.bind(this);
